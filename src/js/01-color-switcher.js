90acf56e2d45c998deb6b4c204359141dc9dcae9
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-07-02 17:13:05 +0300

    solving the first exercise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+"use strict"
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  
+  let intervalId = null;
+  
+  // Funcția de start a schimbării culorii
+  function startColorChange() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  
+    // Pornirea intervalului de schimbare a culorii la fiecare secundă
+    intervalId = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      document.body.style.backgroundColor = randomColor;
+    }, 1000);
+  }
+  
+  function stopColorChange() {
+    stopButton.disabled = true;
+    startButton.disabled = false;
+    clearInterval(intervalId);
+  }
+  
+  startButton.addEventListener('click', startColorChange);
+  stopButton.addEventListener('click', stopColorChange);
