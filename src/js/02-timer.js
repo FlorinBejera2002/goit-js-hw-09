0bc5aab8b780beeb4a9e3e399c9745fb070e218a
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-07-02 18:53:00 +0300

    initial commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,33 @@
-"use strict";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
 const datetimePicker = document.getElementById('datetime-picker');
 const startButton = document.querySelector('[data-start]');
-
 const daysElement = document.querySelector('[data-days]');
 const hoursElement = document.querySelector('[data-hours]');
 const minutesElement = document.querySelector('[data-minutes]');
 const secondsElement = document.querySelector('[data-seconds]');
 
 let countdownInterval;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate <= new Date()) {
+      alert("Please choose a date in the future");
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr(datetimePicker, options);
 
 startButton.addEventListener('click', () => {
   const countdownDate = new Date(datetimePicker.value).getTime();
@@ -17,28 +36,39 @@ startButton.addEventListener('click', () => {
     const now = new Date().getTime();
     const distance = countdownDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    daysElement.textContent = formatTime(days);
-    hoursElement.textContent = formatTime(hours);
-    minutesElement.textContent = formatTime(minutes);
-    secondsElement.textContent = formatTime(seconds);
-
     if (distance < 0) {
       clearInterval(countdownInterval);
+      daysElement.textContent = '00';
+      hoursElement.textContent = '00';
+      minutesElement.textContent = '00';
+      secondsElement.textContent = '00';
+      alert("Countdown finished!");
+      return;
     }
+
+    const { days, hours, minutes, seconds } = convertMs(distance);
+
+    daysElement.textContent = addLeadingZero(days);
+    hoursElement.textContent = addLeadingZero(hours);
+    minutesElement.textContent = addLeadingZero(minutes);
+    secondsElement.textContent = addLeadingZero(seconds);
   }, 1000);
 });
 
-// Inițializarea calendarului Flatpickr
-flatpickr('#datetime-picker', {
-  enableTime: true,
-  dateFormat: 'Y-m-d H:i',
-});
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor((ms % hour) / minute);
+    const seconds = Math.floor((ms % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
 
-function formatTime(time) {
-  return time.toString().padStart(2, '0');
+function addLeadingZero(value) {
+  return String(value).padStart(1, '0');
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const delayInput = form.elements['delay'];
+    const stepInput = form.elements['step'];
+    const amountInput = form.elements['amount'];
+
+    const firstDelay = parseInt(delayInput.value);
+    const step = parseInt(stepInput.value);
+    const amount = parseInt(amountInput.value);
+
+    let currentDelay = firstDelay;
+
+    for (let i = 1; i <= amount; i++) {
+      createPromise(i, currentDelay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+
+      currentDelay += step;
+    }
+
+    // Reset form inputs
+    form.reset();
+  });
+});
