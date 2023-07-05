303585b851301216039a3bce0a6229ebc5ad9505
FlorinBejera2002 <florinpetru0306@gmail.com>
2023-07-05 22:28:12 +0300

    initial commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,55 +1,49 @@
-import Notiflix from 'notiflix';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.form');
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const delayInput = document.querySelector('input[name="delay"]');
-    const stepInput = document.querySelector('input[name="step"]');
-    const amountInput = document.querySelector('input[name="amount"]');
-
-    const delay = Number(delayInput.value);
-    const step = Number(stepInput.value);
-    const amount = Number(amountInput.value);
-
-    if (isNaN(delay) || isNaN(step) || isNaN(amount)) {
-      alert('Please enter valid numbers!');
-      return;
-    }
-
-    createPromises(delay, step, amount);
-  });
-
-  function createPromises(initialDelay, step, amount) {
-    let currentDelay = initialDelay;
-
-    for (let i = 1; i <= amount; i++) {
-      createPromise(i, currentDelay)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-        return;
-
-      currentDelay += step;
-    }
-  }
-
-  function createPromise(position, delay) {
-    return new Promise((resolve, reject) => {
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
 
-      setTimeout(() => {
-        if (shouldResolve) {
-          resolve({ position, delay });
-        } else {
-          reject({ position, delay });
-        }
-      }, delay);
-    });
+function createPromises(initialDelay, step, amount) {
+  let currentDelay = initialDelay;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        displayResult(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        displayResult(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += step;
   }
+}
+
+function displayResult(message) {
+  const resultElement = document.getElementById('result');
+  const messageElement = document.createElement('p');
+  messageElement.textContent = message;
+  resultElement.appendChild(messageElement);
+}
+
+document.querySelector('.form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const initialDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  createPromises(initialDelay, step, amount);
 });
